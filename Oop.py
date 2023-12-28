f9b96954585dbb2fa1a5a463194ce64c15c9aca6
MayaSara <mayasara@localhost.localdomain>
2023-12-28 22:13:16 +0800

    Baru, Muhammad Rezky Ananda

--- Oop.py
+++ Oop.py
@@ -11,8 +11,31 @@ class Objek:
 class Pegawai(Objek):
     def TampilNamaPegawai(self):
         print(f'halo {self.name} anda adalah seorang staf di sini')
+#enkapsulasi
+class password:
+    def __init__(self,username,password):
+        self.__user = username
+        self.__pass = password
+    def TampilInfo(self):
+        print(f'halo {self.__user} anda punya password {self.__pass}')
+#contoh error mengakses enkapsulasi
+# class error(password):
+#     def test(self):
+#         print(f'halo {self.__user}')
+        
+#polymorphism
+class poly:
+    def __init__(self,prodi,univ):
+        self.prodi = prodi
+        self.univ = univ
 
+    def TampilkanPesan(self):
+        print(f'prodi kamu adalah {self.prodi}, dan universitas kami adalah {self.univ}')
 #class objek di panggil
 Objek('panini',12).TampilkanPesan()
+poly('Teknik Informatika','UHO').TampilkanPesan()
 #class Pegawai di panggil
 Pegawai('panini',12).TampilNamaPegawai()
+#class Passowrd di panggil
+password('panini123',123123123).TampilInfo()
+# error('panini123',1111).test()
